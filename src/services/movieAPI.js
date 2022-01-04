e5a3f5dbea2d613857971ed264ee2a2cbbcae00a
Alexey Sapr <alexeysapr@gmail.com>
2022-01-04 12:39:37 +0200

    Add movie api

---
+++ src/services/movieAPI.js
@@ -0,0 +1,94 @@
+import axios from 'axios';
+
+// Ключ API (v3 auth)
+// 93fd20970d74d9a3f9466d8d6c9e6297
+
+// Ключ доступа к API (v4 auth)
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5M2ZkMjA5NzBkNzRkOWEzZjk0NjZkOGQ2YzllNjI5NyIsInN1YiI6IjYxOTNhYjExNDJmMTlmMDA0MzFlZTkzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z6zVAqAKYmCRhBa4xmofDsVBFw9-x8O5I_GnOqiw-F8
+
+const AUTH_KEY =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5M2ZkMjA5NzBkNzRkOWEzZjk0NjZkOGQ2YzllNjI5NyIsInN1YiI6IjYxOTNhYjExNDJmMTlmMDA0MzFlZTkzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z6zVAqAKYmCRhBa4xmofDsVBFw9-x8O5I_GnOqiw-F8';
+axios.defaults.headers.common['Authorization'] = `Bearer ${AUTH_KEY}`;
+
+//экспорт функций запросов
+export default class SearchAPI {
+  #baseUrl = 'https://api.themoviedb.org/3';
+  #page = 1;
+
+  constructor() {
+    this.searchQuery = '';
+  }
+
+  get page() {
+    return this.#page;
+  }
+
+  set page(numOfPage) {
+    this.#page = numOfPage;
+  }
+
+  ressetPage() {
+    this.#page = 1;
+  }
+
+  //запрос на самые популярные фильмы за день
+  async getDayTrandingMovies() {
+    try {
+      const response = await axios.get(`${this.#baseUrl}/trending/movie/day`);
+      const movies = await response.data;
+      return movies;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  //запрос на фильм по ID
+  async getMovieById(movieId) {
+    try {
+      const response = await axios.get(`${this.#baseUrl}/movie/${movieId}`);
+      const movie = await response.data;
+      return movie;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  //поиск кинофильма по ключевому слову на странице фильмов
+  async getMoviesByQuery() {
+    try {
+      const response = await axios.get(
+        `${this.#baseUrl}/search/movie?query=${this.searchQuery}&page=1`,
+      );
+      const movie = await response.data;
+      return movie;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  //запрос информации о актёрском составе для страницы кинофильма
+  async getMovieCredits(movieId) {
+    try {
+      const response = await axios.get(
+        `${this.#baseUrl}/movie/${movieId}/credits`,
+      );
+      const movie = await response.data;
+      return movie;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  //запрос обзоров для страницы кинофильма
+  async getMovieReviews(movieId) {
+    try {
+      const response = await axios.get(
+        `${this.#baseUrl}/movie/${movieId}/reviews?page=1`,
+      );
+      const movie = await response.data;
+      return movie;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
