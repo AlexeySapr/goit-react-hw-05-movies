3edee0521118d1ef6e41c16acaeb616465eb2341
Alexey Sapr <alexeysapr@gmail.com>
2022-01-04 15:57:50 +0200

    Add styles for nav

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,35 +1,21 @@
 import { NavLink } from 'react-router-dom';
 import s from './Navigation.module.css';
-console.log(s);
 
 const Navigation = () => {
-  console.log('props: ', this);
   return (
     <nav>
       <NavLink
         to="/"
-        style={({ isActive }) => {
-          return {
-            textDecoration: 'none',
-            fontWeight: 500,
-            fontSize: 18 + 'px',
-            marginRight: 15 + 'px',
-            color: isActive ? 'darkviolet' : 'black',
-          };
+        className={({ isActive }) => {
+          return isActive ? `${s.navLink} ${s.activeNavLink}` : s.navLink;
         }}
       >
         Home
       </NavLink>
       <NavLink
         to="/movies"
-        style={({ isActive }) => {
-          return {
-            textDecoration: 'none',
-            fontWeight: 500,
-            fontSize: 18 + 'px',
-            marginRight: 15 + 'px',
-            color: isActive ? 'darkviolet' : 'black',
-          };
+        className={({ isActive }) => {
+          return isActive ? `${s.navLink} ${s.activeNavLink}` : s.navLink;
         }}
       >
         Movies
