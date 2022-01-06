489d99c644fe05a7bebdd27af073a77ef604805c
Alexey Sapr <alexeysapr@gmail.com>
2022-01-06 15:03:42 +0200

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
-// import Loader from 'react-loader-spinner';
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
-// import Button from './components/button/Button';
-// import ImageGallery from './components/imageGallery/ImageGallery';
-// import Modal from './components/modal/Modal';
-// import Searchbar from './components/searchbar/Searchbar';
-// // import initData from './data/Data.json';
-// import { searchService } from './services/searchAPI.js';
-
-// import Container from './components/container/Container';
 import Appbar from './components/appbar/Appbar';
 import HomePage from './views/HomePage';
 import MoviesPage from './views/MoviesPage';
@@ -23,14 +11,14 @@ import Reviews from './components/reviews/Reviews';
 const App = () => {
   return (
     <>
-      <Appbar />
-
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="movies" element={<MoviesPage />} />
-        <Route path="movies/:movieID" element={<MovieDetailsPage />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
+        <Route path="/" element={<Appbar />}>
+          <Route index element={<HomePage />} />
+          <Route path="movies" element={<MoviesPage />} />
+          <Route path="movies/:movieID" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </>
--- src/components/appbar/Appbar.js
+++ src/components/appbar/Appbar.js
@@ -1,3 +1,5 @@
+import { Outlet } from 'react-router-dom';
+
 import Navigation from '../navigation/Navigation';
 import Container from '../container/Container';
 
@@ -5,11 +7,15 @@ import s from './Appbar.module.css';
 
 const Appbar = () => {
   return (
-    <header className={s.header}>
-      <Container>
-        <Navigation />
-      </Container>
-    </header>
+    <>
+      <header className={s.header}>
+        <Container>
+          <Navigation />
+        </Container>
+      </header>
+
+      <Outlet />
+    </>
   );
 };
 
